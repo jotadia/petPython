4b9ae5b4e592d0beccad96160f1755837aa6f7f3
JULIO CESAR OTALORA DIAZ <juliotadia@colsubsidio.com>
2026-08-10 16:44:59 -0500

    Calse Mascota

---
+++ adopcion/models/mascota.py
@@ -0,0 +1,9 @@
+class Mascota:
+    def __init__(self, nombre, edad, especie, adoptado=False):
+        self.nombre = nombre
+        self.edad = edad
+        self.especie = especie
+        self.adoptado = adoptado
+
+    def __str__(self):
+        return f"{self.nombre} ({self.especie}, {self.adoptado}, {self.edad} años)"
